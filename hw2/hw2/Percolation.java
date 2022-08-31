64977d1e441e5379af09d004e8f86c43f6dc0dbf
username-31 <85339089+username-31@users.noreply.github.com>
2022-08-31 10:22:22 +0800

    hw2 done

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,128 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private WeightedQuickUnionUF sites;
+    private boolean[] openCheck;
+    private boolean[] fullCheck; // TODO: Do we really need this? Maybe it's helpful when gravity comes in!
+    private int N;
+     private int numberOfOpenSites;
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.N = N;
+        numberOfOpenSites = 0;
+        sites = new WeightedQuickUnionUF(N * N + 2);
+        openCheck = new boolean[N * N + 2];
+        fullCheck = new boolean[N * N + 2];
+        for (int i = 0; i < N * N; i++) {
+            openCheck[i] = false;
+            fullCheck[i] = false;
+        }
+
+        /**
+         *  the N * N -th item is the virtual top site,
+         *  the N * N + 1 -th item is the virtual bottom site.
+         */
+        openCheck[N * N] = true;
+        fullCheck[N * N] = true;
+        openCheck[N * N + 1] = false;
+        fullCheck[N * N + 1] = false;
+    }
+
+    private int xyTo1D(int row, int col) {
+
+        return N * row + col;
+    }
+
+    public void open(int row, int col) { // open the site (row, col) if it is not open already
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (isOpen(row, col)) {
+            return;
+        }
+        int self = xyTo1D(row, col);
+        int up = xyTo1D(row - 1, col);
+        int down = xyTo1D(row + 1, col);
+        int left = xyTo1D(row, col - 1);
+        int right = xyTo1D(row, col + 1);
+
+        boolean notTopmost = (row != 0);
+        boolean notLeftmost = (col != 0);
+        boolean notRightmost = (col != N - 1);
+        boolean notBottom = (row != N - 1);
+
+        boolean leftIsFull = false;
+        boolean rightIsFull = false;
+        boolean upIsFull = false;
+
+        openCheck[xyTo1D(row, col)] = true;
+        numberOfOpenSites += 1;
+
+        if (row == 0) {
+            sites.union(self, N * N);
+        }
+        if (notTopmost) {
+            if (isOpen(row - 1, col)) {
+                sites.union(up, self);
+                upIsFull = isFull(row - 1, col);
+            }
+        }
+
+        if (notLeftmost) {
+            if (isOpen(row, col - 1)) {
+                sites.union(left, self);
+                leftIsFull = isFull(row, col - 1);
+            }
+        }
+        if (notRightmost) {
+            if (isOpen(row, col + 1)) {
+                sites.union(right, self);
+                rightIsFull = isFull(row, col + 1);
+            }
+        }
+
+        boolean hasFullNeighbor = (leftIsFull || upIsFull || rightIsFull);
+        if (notBottom) {
+            if (isOpen(row + 1, col)) {
+                sites.union(down, self);
+            }
+        }
+
+        if (!notBottom) {
+            sites.union(N * N + 1, self);
+        }
+
+    }
+
+    public boolean isOpen(int row, int col) { // is the site (row, col) open?
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new IndexOutOfBoundsException();
+        }
+        return openCheck[xyTo1D(row, col)];
+    }
+
+    public boolean isFull(int row, int col) { // is the site (row, col) full?
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new IndexOutOfBoundsException();
+        }
+        return sites.connected(xyTo1D(row, col), N * N);
+    }
+
+    public int numberOfOpenSites() { // number of open sites
+        return numberOfOpenSites;
+    }
+
+    public boolean percolates() { // does the system percolate?
+        return sites.connected(N * N, N * N + 1);
+    }
+
+    public static void main(String[] args) { // use for unit testing (not required, but keep this here for the autograder)
+
+        Percolation p = new Percolation(5);
+
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,53 @@
 package hw2;
 
-public class PercolationStats {
+import org.junit.Test;
+import edu.princeton.cs.introcs.StdStats;
+import edu.princeton.cs.introcs.StdRandom;
 
+public class PercolationStats {
+    int N;
+    int T;
+    double[] x;
+    Percolation[] perc;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) { // perform T independent experiments on an N-by-N grid
+        this.T = T;
+        this.N = N;
+        this.x = new double[T];
+        this.perc = new Percolation[T];
+        for (int i = 0; i < T; i++) {
+            perc[i] = pf.make(N);
+            while (!perc[i].percolates()) {
+                perc[i].open(StdRandom.uniform(0, N), StdRandom.uniform(0, N));
+            }
+        }
+    }
+
+    public double mean() { // sample mean of percolation threshold
+        for (int i = 0; i < T; i++) {
+            x[i] = 1.0 * perc[i].numberOfOpenSites() / (N * N);
+        }
+
+        return StdStats.mean(x);
+    }
+
+    public double stddev() { // sample standard deviation of percolation threshold
+        return StdStats.stddev(x);
+    }
+
+    public double confidenceLow() { // low endpoint of 95% confidence interval
+        return mean() - (1.96 * stddev() / Math.sqrt(T));
+    }
+
+    public double confidenceHigh() { // high endpoint of 95% confidence interval
+        return mean() + (1.96 * stddev() / Math.sqrt(T));
+    }
+
+    public static void main(String[] args) {
+        PercolationStats ps1 = new PercolationStats(10, 10000, new PercolationFactory());
+        System.out.println(ps1.mean());
+        System.out.println(ps1.stddev());
+        System.out.println(ps1.confidenceLow());
+        System.out.println(ps1.confidenceHigh());
+    }
 }
